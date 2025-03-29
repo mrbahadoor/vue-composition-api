0a9b538f764738b4114f149ea1ab26d9f87262fa
Rushdi Bahadoor <my_name@example.com>
2025-03-29 15:40:58 +0400

    Composition api - basics

---
+++ basics/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="increment">Center</button>
+    <div>{{ counter }}</div>
+    <div>{{ arrayOfEmojis }}</div>
+  </div>
+</template>
+<script>
+  import { ref, onMounted, watch, computed } from 'vue'
+
+  export default {
+    props: {
+      emoji: {type: String, default: '🔥' }
+    },
+    setup(props) {
+       console.log(props.emoji);
+
+       onMounted(() => {
+        console.log('Counter component has been mounted')
+       })
+
+       const counter = ref(0)
+
+       const increment = () => counter.value++
+
+       watch(counter, current => {
+        if (current === 5) console.log('You have clicked five times!')
+       })
+
+       const arrayOfEmojis = computed(() => {
+        Array.from(new Array(counter.value, () => props.emoji)).join(' ')
+       })
+       
+
+       return {counter, increment, arrayOfEmojis}
+    }
+  }
+</script>
+<style scoped>
+
+</style>
