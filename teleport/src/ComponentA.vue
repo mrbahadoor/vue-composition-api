6b2c97e1f2897e1f8f0f390bac474488dbb465e9
Rushdi Bahadoor <my_name@example.com>
2025-03-30 21:55:58 +0400

    Teleport

---
+++ teleport/src/ComponentA.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card">
+        Component A
+        <button @click="changeToggle">Toggle</button>
+        <teleport to="#endofbody" :disabled="toggle">
+            <p>Text from component A</p>
+        </teleport>
+    </div>
+</template>
+<script>
+    import { ref } from 'vue';
+    export default {
+        setup() {            
+            const toggle = ref(false)
+            
+            const changeToggle = () => { 
+                toggle.value = !toggle.value;
+                console.log('toggle', toggle.value);
+            } 
+
+            return {
+                toggle,
+                changeToggle
+            }            
+        }
+    }
+</script>
+<style scoped>
+    .card {
+        display: table;
+        margin: 20px auto;
+        padding: 20px;
+        border: green solid 1px;
+    }
+</style>
