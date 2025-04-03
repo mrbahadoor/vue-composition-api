5c4b5b2fbac395a26ed1ef1f24a170c2090c0718
Rushdi Bahadoor <my_name@example.com>
2025-04-04 01:32:43 +0400

    Provide - inject

---
+++ provide-inject/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <SettingsProvider>
+        <ThemeButton />
+    </SettingsProvider>
+</template>
+<script>
+    import SettingsProvider from './SettingsProvider.vue'
+    import ThemeButton from './ThemeButton.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            SettingsProvider,
+            ThemeButton
+        }
+    }
+</script>
---
+++ provide-inject/src/ThemeButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button :class="{ 'dark-mode': settings.theme === 'dark' }" @click="toggleMode">Switch to {{ oppositeTheme }} mode</button>
+</template>
+
+<script>
+    import { computed, inject } from 'vue'
+    export default {
+        name: 'ThemeButton',
+        setup() {
+            const settings = inject('settings')
+        
+            const oppositeTheme = computed(() => {
+                return settings.theme === 'light' ? 'dark' : 'light'
+            })
+
+            const toggleMode = () => {
+                settings.theme = oppositeTheme.value
+            }
+
+            return { settings, oppositeTheme, toggleMode }
+        }
+    }
+</script>
+<style scoped>
+    .dark-mode {
+        color: white;
+        background-color: black;
+    }
+</style>
+
+
